f8c5f7bdce8f84898fe95b0f49c105c909b43fb6
Aymen maaloul <103130221+Aymen-maaloul@users.noreply.github.com>
2023-07-17 15:13:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".a2").click(function(){
+        $(".menu").fadeIn(500);
+    });
+    $(".out").click(function(){
+        $(".menu").fadeOut(500);
+    });
+});
