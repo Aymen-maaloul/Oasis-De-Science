10fbf57d3e6463871ff312ed3677de063f9ea4a8
Aymen maaloul <103130221+Aymen-maaloul@users.noreply.github.com>
2023-07-31 17:09:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_pfivuzk";
+    const templateID = "template_999brkj";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          window.open("thank you.html")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
